64eb6e399736867d516e28eb8b5105b53560d0c9
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-13 13:12:38 +0000

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+  let[fruit,setFruit]=useState("");
+  let[filtered,setFiltered]=useState(fruits)
+  useEffect(()=>{
+    setFiltered(fruits.filter((value)=>value.includes(fruit)))
+  },[fruit])
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input type="text" onChange={(e)=>setFruit(e.target.value)}></input>
+        <ul>
+        {
+          filtered.map((value)=>{
+            <li>{value}</li>
+          })
+        }
+        </ul>
     </div>
   )
 };

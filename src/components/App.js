0767baf4368be16fa69dcb49d35ff54eeb0b266f
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-13 13:35:34 +0000

    sec

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,46 @@ import './../styles/App.css';
 
 const App = () => {
   const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
-  let[fruit,setFruit]=useState("");
-  let[filtered,setFiltered]=useState(fruits)
-  useEffect(()=>{
-    setFiltered(fruits.filter((value)=>value.includes(fruit)))
-  },[fruit])
+  
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChange = (event) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+
+    // Delay the execution of suggestions update to prevent UI freezes
+    setTimeout(() => {
+      updateSuggestions(newSearchTerm);
+    }, 300);
+  };
+
+  const updateSuggestions = (newSearchTerm) => {
+    if (newSearchTerm.trim() === '') {
+      setSuggestions([]);
+    } else {
+      const filteredSuggestions = fruits.filter((fruit) =>
+        fruit.toLowerCase().includes(newSearchTerm.toLowerCase())
+      );
+      setSuggestions(filteredSuggestions);
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>Search Item</h1>
-        <input type="text" onChange={(e)=>setFruit(e.target.value)}></input>
-        <ul>
-        {
-          filtered.map((value)=>{
-            <li>{value}</li>
-          })
-        }
-        </ul>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Search fruits..."
+      />
+      <ul>
+        {suggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 };
 
 export default App
